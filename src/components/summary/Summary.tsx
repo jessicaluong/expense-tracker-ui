437920d586e425e9037cf2a431ebda45db48cb2e
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2025-07-07 18:00:17 -0700

    feat(view expenses): display total expenses in Summary

--- src/components/summary/Summary.tsx
+++ src/components/summary/Summary.tsx
@@ -1,14 +1,17 @@
 import { useExpensesContext } from "@/lib/hooks";
 import SummaryCard from "./SummaryCard";
+import { currencyFormat } from "@/lib/utils";
 
 export default function Summary() {
   const { expenses } = useExpensesContext();
 
   const categoryTotals: Record<string, number> = {};
+  let grandTotal: number = 0;
 
   for (const expense of expenses) {
     categoryTotals[expense.category] =
       (categoryTotals[expense.category] || 0) + expense.amount;
+    grandTotal += expense.amount;
   }
 
   return (
@@ -17,6 +20,9 @@ export default function Summary() {
       {Object.entries(categoryTotals).map(([category, total]) => (
         <SummaryCard key={category} category={category} total={total} />
       ))}
+      <p className="text-xl font-semibold">
+        Total: {currencyFormat(grandTotal)}
+      </p>
     </div>
   );
 }
